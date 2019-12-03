4f9cfadf08d2a3ad8ba5d01eca9e82f3a931d997
manzartoflo <manzar.toflo@gmail.com>
2019-12-04 00:58:42 +0530

    register

---
+++ validate.js
@@ -0,0 +1,7 @@
+name = document.getElementById('name');
+username = document.getElementById('username');
+password = document.getElementById('password');
+confirm_password = document.getElementById('confirm-password');
+
+
+
