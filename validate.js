8255153fbebc8370677fe14bcd4d55a99ce1e163
manzartoflo <manzar.toflo@gmail.com>
2019-12-05 14:40:48 +0530

    validating

--- validate.js
+++ validate.js
@@ -1,7 +1,29 @@
-name = document.getElementById('name');
-username = document.getElementById('username');
-password = document.getElementById('password');
-confirm_password = document.getElementById('confirm-password');
 
+function validate(){
+    var name = document.getElementById('name');
+    var username = document.getElementById('username');
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+    var confirm_password = document.getElementById('confirm-password');
 
-
+     if (name.value == "" ){
+        document.getElementById("inc-name").innerHTML="Name field cannot be empty!"
+        return false;
+     }
+    if (username.value == "") {
+        document.getElementById("inc-name").innerHTML = "Name field cannot be empty!"
+        return false;
+    }
+    if (.value == "") {
+        document.getElementById("inc-name").innerHTML = "Name field cannot be empty!"
+        return false;
+    }
+    if (username.value == "") {
+        document.getElementById("inc-name").innerHTML = "Name field cannot be empty!"
+        return false;
+    }
+    if (username.value == "") {
+        document.getElementById("inc-name").innerHTML = "Name field cannot be empty!"
+        return false;
+    }
+}
