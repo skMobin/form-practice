d1f26f2b4d19ef4411a91c69fba5f53bad73ad85
manzartoflo <manzar.toflo@gmail.com>
2019-12-14 18:26:05 +0530

    validating

--- validate.js
+++ validate.js
@@ -1,29 +1,55 @@
-
 function validate(){
-    var name = document.getElementById('name');
-    var username = document.getElementById('username');
-    var email = document.getElementById('email');
-    var password = document.getElementById('password');
-    var confirm_password = document.getElementById('confirm-password');
+        var name = document.getElementById('name');
+        var username = document.getElementById('username');
+        var email = document.getElementById('email');
+        var password = document.getElementById('pass');
+        var confirm_password = document.getElementById('con-pass');
+        if (name.value.trim() == "") {
+            document.getElementById("inc-name").innerHTML = "Name field cannot be empty!";
+            return false;
+        
+        }
+        else{
+            document.getElementById("inc-name").innerHTML = "";
+        }
+         if (username.value.trim() == "") {
+            document.getElementById("inc-username").innerHTML = "Username field cannot be empty!";
+            return false;
+        }
+        else if (username.value.trim().length < 7) {
+            alert("!!!!");
+            document.getElementById("inc-username").innerHTML = "Username should be more than 6 characters !";
+            return false;
+        }
+        else{
+             document.getElementById("inc-username").innerHTML = "";
 
-     if (name.value == "" ){
-        document.getElementById("inc-name").innerHTML="Name field cannot be empty!"
-        return false;
-     }
-    if (username.value == "") {
-        document.getElementById("inc-name").innerHTML = "Name field cannot be empty!"
-        return false;
-    }
-    if (.value == "") {
-        document.getElementById("inc-name").innerHTML = "Name field cannot be empty!"
-        return false;
-    }
-    if (username.value == "") {
-        document.getElementById("inc-name").innerHTML = "Name field cannot be empty!"
-        return false;
-    }
-    if (username.value == "") {
-        document.getElementById("inc-name").innerHTML = "Name field cannot be empty!"
-        return false;
-    }
+        }
+         if (email.value.trim() == "") {
+            document.getElementById("inc-email").innerHTML = "Email field cannot be empty!";
+            return false;
+        }
+        else
+        {
+             document.getElementById("inc-email").innerHTML = "";
+
+        }
+         if (password.value.trim() == "") {
+            document.getElementById("inc-pass").innerHTML = "Password field cannot be empty!";
+            return false;
+        }
+        else{
+             document.getElementById("inc-pass").innerHTML = "";
+
+        }
+         if (confirm_password.value == "") {
+            document.getElementById("inc-cpass").innerHTML = "Confirm Password field cannot be empty!";
+            return false;
+        }
+        else{
+             document.getElementById("inc-cpass").innerHTML = "";
+
+        }
+        return true;
+
 }
